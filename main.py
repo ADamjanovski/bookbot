972d3c3e7180115b9322b5e2eedd5bfdb540eb62
Damjanovski <andrej-damjanovski@hotmail.com>
2025-10-02 18:55:24 +0200

    Five

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ def read_book(path_to_file):
     with open(path_to_file) as f:
         print(f.read())
 
+def count_words(path_to_file):
+    with open(path_to_file) as f:
+        count = f.read().split().__len__()
+        print(f"Found {count} total words")
+
 
 if __name__ == "__main__":
     args = sys.argv
-    read_book("./books/frankenstein.txt")
+    count_words("./books/frankenstein.txt")
